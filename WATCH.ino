1f7fe9f9c4f97ca1989cd3671d523382778821dc
Sanjhay12 <99126901+Sanjhay12@users.noreply.github.com>
2023-03-03 20:17:29 +0000

    Add files via upload

---
+++ WATCH.ino
@@ -0,0 +1,270 @@
+/**
+ * A BLE client example that is rich in capabilities.
+ * There is a lot new capabilities implemented.
+ * author unknown
+ * updated by chegewara
+ */
+
+#include "BLEDevice.h"
+//#include "BLEScan.h"
+#include <WiFi.h>
+#include "time.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+const char* ssid     = "Galaxy S21 Ultra 5G0cfb";
+const char* password = "hehe1234";
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+
+long start_time = 0;
+String last_time = "";
+int last_duration;
+long prev_screen = 0;
+String currentTime = "";
+volatile int hall_value = 0;
+bool screen_page = false;
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("1811");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("2A3F");
+static BLEUUID    charUUID1("2A40");
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLERemoteCharacteristic* pRemoteCharacteristic1;
+static BLEAdvertisedDevice* myDevice;
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    //Serial.print("Notify callback for characteristic ");
+    //Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    //Serial.print(" of data length ");
+    //Serial.println(length);
+    //Serial.print("data: ");
+    //Serial.println(pData[0]);
+    hall_value = pData[0];
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    //Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+    //Serial.print("Forming a connection to ");
+    //Serial.println(myDevice->getAddress().toString().c_str());
+    
+    BLEClient*  pClient  = BLEDevice::createClient();
+    //Serial.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+    
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    //Serial.println(" - Connected to server");
+    pClient->setMTU(517); //set client to request maximum MTU from server (default is 23 otherwise)
+  
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      //Serial.print("Failed to find our service UUID: ");
+      //Serial.println(serviceUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    //Serial.println(" - Found our service");
+
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    pRemoteCharacteristic1 = pRemoteService->getCharacteristic(charUUID1);
+    if (pRemoteCharacteristic == nullptr) {
+      //Serial.print("Failed to find our characteristic UUID: ");
+      //Serial.println(charUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    //Serial.println(" - Found our characteristic");
+    
+    if(pRemoteCharacteristic->canRead()) {
+      std::string value = pRemoteCharacteristic->readValue();
+      //Serial.print("The characteristic value was: ");
+      //Serial.println(value.c_str());
+    }
+
+    if(pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+      
+    connected = true;
+    return true;
+}
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+ /**
+   * Called for each advertising BLE server.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    //Serial.print("BLE Advertised Device found: ");
+    //Serial.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+
+    } // Found our server
+  } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+
+void setup() {
+  Serial.begin(115200);
+  //Serial.println("Starting Arduino BLE Client application...");
+  //Serial.print("Connecting to ");
+  //Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //Serial.print(".");
+  }
+  //Serial.println("");
+  //Serial.println("WiFi connected.");
+  pinMode(19, OUTPUT);
+  
+  digitalWrite(19, LOW);
+  pinMode(4, INPUT_PULLUP);
+  
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  BLEDevice::init("");
+   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    //Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+
+  delay(2000);
+  display.display();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    connectToServer();
+    prev_screen = millis();
+    doConnect = false;
+  }
+
+  if (connected) {
+    if(millis() - prev_screen > 3000){
+      screen_page = !screen_page;
+      prev_screen = millis();
+    }
+
+    if(!screen_page){
+      LCD_display();
+    }
+
+    else{
+      display_memory(last_duration);
+    }
+    
+    //Serial.println(hall_value);
+    if(hall_value == 1){
+      start_time = millis();
+      last_time = currentTime;
+      digitalWrite(19, HIGH);
+    }
+    while (hall_value == 1){
+      int time_elapsed = (millis()-start_time)/1000;
+      //Serial.println(time_elapsed);
+      display_memory(time_elapsed);
+      //Serial.println("Awaiting reset!");
+      if(digitalRead(4)==0){  
+        pRemoteCharacteristic1 -> writeValue(1, sizeof(1));
+        digitalWrite(19, LOW);
+        last_duration = time_elapsed;
+     }
+    }
+  }
+  else if(doScan){
+    BLEDevice::getScan()->start(0);  // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+  
+  delay(500); // Delay a second between loops.
+} // End of loop
+
+
+void LCD_display(){  
+  display.clearDisplay();
+  display.setCursor(10,0);
+  struct tm timeinfo;
+  if ( getLocalTime(&timeinfo)== true) //Updates the time variables from RTC
+  {
+    char timeStringBuff[50]; //50 chars should be enough
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo); 
+    currentTime = (String) timeStringBuff;
+    display.println(currentTime);
+    display.display();
+  }
+}
+
+
+void display_memory(int time_elapsed){
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(last_time);
+  display.println("Your bag was open for "+(String)time_elapsed + "sec");
+  display.display();
+}
